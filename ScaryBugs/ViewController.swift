f0dab766de42fa2ab90b3d34a8c9c2036150cc5b
ElijahButers <Elijahbuters@gmail.com>
2016-07-23 14:03:54 +0300

    Added code to ViewController and viewDidLoad() that creates an instance of ScaryBug class.

--- ScaryBugs/ViewController.swift
+++ ScaryBugs/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var bugs = [ScaryBug]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        bugs = ScaryBug.bugs()
     }
 
     override func didReceiveMemoryWarning() {
