a5c12a217417bf1fc23abf452d5e13a8e561085c
ElijahButers <Elijahbuters@gmail.com>
2016-08-03 18:49:38 +0300

    Replaced code in viewDidLoad() that calls a new setupBugs() method.

--- ScaryBugs/ViewController.swift
+++ ScaryBugs/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //bugs = ScaryBug.bugs()
+        setupBugs()
     }
 
     override func didReceiveMemoryWarning() {
