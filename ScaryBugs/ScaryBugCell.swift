5a4a32efe390733f75ffa4ab5c80d2d371e9f726
ElijahButers <Elijahbuters@gmail.com>
2016-08-15 14:36:02 +0300

    Added ScaryBugCell class, a subclass of UITableViewCell to the project.

---
+++ ScaryBugs/ScaryBugCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScaryBugCell.swift
+//  ScaryBugs
+//
+//  Created by User on 8/15/16.
+//  Copyright © 2016 Elijah Buters. All rights reserved.
+//
+
+import UIKit
+
+class ScaryBugCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
